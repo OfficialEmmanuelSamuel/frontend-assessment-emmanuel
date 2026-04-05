f809248177bbfb099132a8e729e86e5fce7002e7
OfficialEmmanuelSamuel <official.emmanuel.samuel@gmail.com>
2026-04-05 03:59:35 +0100

    feat(types): define shared TMDB movie and response TypeScript types

---
+++ types/movie.ts
@@ -0,0 +1,15 @@
+export type Movie = {
+  id: number;
+  title: string;
+  poster_path: string | null;
+  backdrop_path?: string | null;
+  vote_average: number;
+  release_date: string;
+  overview?: string;
+};
+
+export type MoviesResponse = {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+};
